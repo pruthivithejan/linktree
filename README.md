3631f763a90f81ff22fb78aaefa0409c2ed1b681
Pruthivi Thejan <pruthivithejan.code@gmail.com>
2023-01-07 22:05:14 +0530

    astro

---
+++ README.md
@@ -0,0 +1,12 @@
+Pre-requirements :
+Node.js and npm installed on your pc
+
+Step 1: Download or Clone the repository
+Step 2: Run <i>`npm install`</i> install the dependencies
+Step 3: Run <i>`npm run dev`</i> to start the server
+Step 4: Now open the <i>content.js</i> file and replace the data to your own.
+
+<h3>If you want to learn Astro :</h3>
+Here is the official documentation : https://docs.astro.build/en/getting-started/
+Here is a really good crash course (from Coding in Public): https://www.youtube.com/watch?v=zrPVTf761OI
+Here is a project course (from Coding in Public) : https://www.youtube.com/watch?v=F2pw1C9eKXw&list=PLoqZcxvpWzzeRwF8TEpXHtO7KYY6cNJeF
---
+++ src/content.js
@@ -0,0 +1,53 @@
+const cards = [
+    {
+     href: "https://mobile.twitter.com/pruthivithejan",
+     icon: 'bx bxl-twitter',
+     title: 'Twitter' ,
+     description : 'I care about Twitter, I hope Elon Musk will not ruin it.',
+     class: 'button twitter',
+    },
+    {
+     href: "https://www.instagram.com/pruthivithejan/",
+     icon: 'bx bxl-instagram',
+     title: 'Instagram' ,
+     description : 'When I take good photos, I post it here.',
+     class: 'button instagram',
+    },
+    {
+     href: "https://www.reddit.com/user/pruthivithejan/",
+     icon: 'bx bxl-reddit',
+     title: 'Reddit' ,
+     description : 'For most Q&A, I use reddit.',
+     class: 'button reddit',
+    },
+    {
+     href: "https://t.me/pruthivithejan/",
+     icon: 'bx bxl-telegram',
+     title: 'Telegram' ,
+     description : 'Telegram is my most used messaging app.',
+     class: 'button telegram',
+    },
+    {
+     href: "https://link.friendspire.com/2TCHVXcy8ub",
+     icon: 'bx bx-movie',
+     title: 'Friendspire' ,
+     description : 'Movies, TV Shows, Books and Podcasts are rated here.',
+     class: 'button friendspire',
+    },   
+    {
+     href: "https://open.spotify.com/user/cipqdclr942xwcn7tdeo14o06?si=14e86fca3b8e462d",
+     icon: 'bx bxl-spotify',
+     title: 'Spotify' ,
+     description : 'My Spotify playlists are here.',
+     class: 'button spotify',
+    },   
+    {
+     href: "https://github.com/pruthivithejan",
+     icon: 'bx bxl-github',
+     title: 'Github' ,
+     description : 'I upload all my codes here.',
+     class: 'button github',
+    },         
+];
+
+export default cards;
